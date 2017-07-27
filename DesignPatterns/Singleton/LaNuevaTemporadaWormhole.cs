bfcb15e2d2eddcc59fb1bb87bd0f94b09b63abbf
Simon Jones <simon.jones@ribaenterprises.com>
2017-07-27 10:22:52 +0100

    Added singleton

---
+++ DesignPatterns/Singleton/LaNuevaTemporadaWormhole.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public sealed class LaNuevaTemporadaWormhole
+    {
+        private static readonly object _lockObject = new object();
+
+        private static LaNuevaTemporadaWormhole _instance;
+
+        private LaNuevaTemporadaWormhole()
+        {
+        }
+
+        public static LaNuevaTemporadaWormhole Instance
+        {
+            get
+            {
+                lock (_lockObject)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LaNuevaTemporadaWormhole();
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+    }
+}
